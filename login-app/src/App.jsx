e97e6634e1c44eb9b49b6de3ea9c3929ef6b43e9
Rodrigo <pittezr.dev@gmail.com>
2023-07-10 11:43:17 -0300

    first commit

---
+++ login-app/src/App.jsx
@@ -0,0 +1,12 @@
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
